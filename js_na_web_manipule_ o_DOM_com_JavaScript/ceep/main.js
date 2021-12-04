48b4e872bbb9379b9b4b031bfe7dc1d560d38ef3
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-04 18:16:01 -0300

    Add a funcionalidade de limpar todas as tarefas.

--- js_na_web_manipule_ o_DOM_com_JavaScript/ceep/main.js
+++ js_na_web_manipule_ o_DOM_com_JavaScript/ceep/main.js
@@ -1,9 +1,13 @@
 import { handleNovoItem } from "./componentes/criaTarefa.js";
 import { caregaTarefa } from "./componentes/carregaTarefa.js";
+import { limpaTarefas } from "./componentes/limpaTodasAsTarefas.js";
 
 
-const novaTarefa = document.querySelector('[data-form-button]');
+const novaTarefa = document.querySelector('[data-form-button-new]');
+const limpaTarefa = document.querySelector('[data-form-button-clear]');
 
 novaTarefa.addEventListener('click', handleNovoItem);
+
+limpaTarefa.addEventListener('click', limpaTarefas);
 
 caregaTarefa()
